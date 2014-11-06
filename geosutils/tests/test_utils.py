33b9b5ea825cfce32f8503070a8e727e35943b3e
Lou Markovski <lou.markovski@gmail.com>
2014-11-06 15:01:53 +1100

    task/GDT-560-last-file-processed
    geosutils/utils.py
    * move in the reverse timestamp creation, get_reverse_timestamp
      from the geoutils project

--- geosutils/tests/test_utils.py
+++ geosutils/tests/test_utils.py
@@ -4,7 +4,8 @@
 """
 import unittest2
 
-from geosutils.utils import hashcode
+from geosutils.utils import (hashcode,
+                             get_reverse_timestamp)
 
 
 class TestUtils(unittest2.TestCase):
@@ -18,3 +19,36 @@ class TestUtils(unittest2.TestCase):
         expected = 1182110976
         msg = 'Hash code generation error'
         self.assertEqual(received, expected, msg)
+
+    def test_get_reverse_timestamp(self):
+        """Generate reverse timestamp.
+        """
+        received = get_reverse_timestamp('19961217102630')
+        expected = '09222521218464775807'
+        msg = 'Reverse timestamp error'
+        self.assertEqual(received, expected, msg)
+
+        newer = get_reverse_timestamp('19971217102630')
+        msg = 'Reverse timestamp of newer date should be less'
+        self.assertLess(int(newer), int(received), msg)
+
+    def test_get_reverse_timestamp_current_time(self):
+        """Generate reverse timestamp: current time.
+        """
+        received = get_reverse_timestamp()
+        msg = 'Reverse timestamp (current time) error'
+        self.assertIsNotNone(received, msg)
+
+    def test_get_reverse_timestamp_invalid_source_utc_length(self):
+        """Generate reverse timestamp: invalid source UTC length.
+        """
+        received = get_reverse_timestamp('199612171026')
+        msg = 'Reverse timestamp error: invalid UTC length'
+        self.assertIsNone(received, msg)
+
+    def test_get_reverse_timestamp_invalid_source_unknown_delimeter(self):
+        """Generate reverse timestamp: invalid source (unknown delimiter).
+        """
+        received = get_reverse_timestamp('1996121710--')
+        msg = 'Reverse timestamp error: invalid source (unknown delim)'
+        self.assertIsNone(received, msg)
